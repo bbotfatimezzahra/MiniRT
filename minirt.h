a0a6adc048e69b0f70a3a58f30177ea5a5445d44
Soukaina Nid Bella <nidbellasoukaina@gmail.com>
2025-03-14 23:54:57 +0000

    all is free and lovely

--- minirt.h
+++ minirt.h
@@ -28,8 +28,8 @@
 # define GREEN 0x0000FF00
 # define RED 0xFF000000
 
-# define DIS_WIDTH 10
-# define DIS_LENGTH 10
+# define DIS_WIDTH 200
+# define DIS_LENGTH 200
 # define VECTOR 0
 # define POINT 1
 # define COLOR 2
